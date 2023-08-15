573758f05f40ffefcfb8f421cf0706aa2b88b47f
kifle23 <kiflekifle23@gmail.com>
2023-08-15 15:02:20 +0300

    Config route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/hello'
+  # get 'pages/hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#hello"
 end
